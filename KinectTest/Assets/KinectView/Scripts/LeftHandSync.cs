f9cc5956694fc7eafc8341aa7de540af49eacb4f
onion-ring <alex.ringlein@gmail.com>
2015-11-07 00:35:51 -0500

    whoops, now everything is added

---
+++ KinectTest/Assets/KinectView/Scripts/LeftHandSync.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using Windows.Kinect;
+
+public class LeftHandSync : MonoBehaviour {
+
+	public GameObject leftSphere;
+	public GameObject rightSphere;
+	public GameObject bv;
+	public GameObject bm;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Transform leftObj = bv.GetComponent<BodySourceView> ().jointObjs [7];
+		Transform rightObj = bv.GetComponent<BodySourceView> ().jointObjs [11];
+		if (rightObj != null) {
+			Vector3 right = rightObj.position;
+			rightSphere.transform.position = right;
+		}
+		if (leftObj != null) {
+			Vector3 left = leftObj.position;
+			leftSphere.transform.position = left;
+		}
+	}
+}
