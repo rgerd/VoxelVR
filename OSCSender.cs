983d1c96e41df54d9bf7a3c88a54e8240359c9b6
Robert Gerdisch <rgerd@users.noreply.github.com>
2015-11-18 15:27:28 -0500

    Update OSCSender.cs

--- OSCSender.cs
+++ OSCSender.cs
@@ -1,35 +1,30 @@
 using UnityEngine;
 using System.Collections;
 
-private Osc oscHandler = null;
-public String remoteIp;
-public int sendToPort;
-public int listenerPort;
-public GameObject BodyView;
-
-
 public class OSCSender : MonoBehaviour {
-	// Start is called just before any of the Update methods is called the first time.
+	public string remoteIp = "127.0.0.1";
+	public int sendToPort = 9000;
+	public int listenerPort = 8000;
+	private Osc handler = null;
+	
 	void Start() {
 		UDPPacketIO udp = (UDPPacketIO) GetComponent("UDPPacketIO");
 		udp.init(remoteIp, sendToPort, listenerPort);
-		
-		oscHandler = (Osc) GetComponent("Osc");
-		oscHandler.init(udp);
+		handler = (Osc) GetComponent("Osc");
+		handler.init(udp);
 		//oscHandler.SetAddressHandler("/1/push1", Example);
 	}
 	
 	void Update() {
-		String message = BodyView.GetComponent("BodySourceView").getString();
+		string message = "";
 		OscMessage oscM = null;
 		oscM = Osc.StringToOscMessage("/" + message);
-		oscHandler.Send(oscM);
+		handler.Send(oscM);
 	}
 
 	void OnDisable() {
-		// close OSC UDP socket
-		Debug.Log("closing OSC UDP socket in OnDisable");
-		oscHandler.Cancel();
-		oscHandler = null;
+		Debug.Log("Closing OSC UDP socket in OnDisable");
+		handler.Cancel();
+		handler = null;
 	}
 }
