1a3151190a202316f5bb6efae699cd9b7459347c
Robert Gerdisch <rgerd@users.noreply.github.com>
2015-11-18 15:30:23 -0500

    Update DisableOnStart.cs

--- DisableOnStart.cs
+++ DisableOnStart.cs
@@ -2,10 +2,5 @@ using UnityEngine;
 using System.Collections;
 
 public class DisableOnStart : MonoBehaviour {
-
-    // Use this for initialization
-    void Start () 
-    {
-        gameObject.SetActive (false);
-    }
+    void Start () { gameObject.SetActive (false); }
 }
